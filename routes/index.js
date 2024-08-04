fac5299dc29fcce2f7333fc4b8da9e53c0cd8f72
Xavier N <x.naidet@gmail.com>
2024-08-04 17:24:38 +0200

    Change index.js for mongoose

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+require('../models/connection');
+
+const Pusher = require('pusher');
+const pusher = new Pusher({
+  appId: process.env.PUSHER_APPID,
+  key: process.env.PUSHER_KEY,
+  secret: process.env.PUSHER_SECRET,
+  cluster: process.env.PUSHER_CLUSTER,
+  useTLS: true,
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
